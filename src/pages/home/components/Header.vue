88f8ab91d6986bfafc86568b201892db9017bf6c
cxw-wan <wangwan520>
2018-10-12 11:52:46 +0800

    add home header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header_left">
+      左
+    </div>
+
+    <div class="header_input">
+      中
+    </div>
+
+    <div class="header_right">
+      右
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeHeader'
+
+
+
+}
+</script>
+
+<style scoped>
+.header{
+  height: .88rem;
+  line-height: .88rem;
+  background-color: red;
+  display: flex;
+}
+.header_left{
+  float: left;
+  width: .60rem;
+}
+.header_input{
+
+   flex: 1;
+  background-color: azure;
+  border-radius: 3px;
+  height: .64rem;
+  margin-top: .12rem;
+  line-height: .60rem;
+
+}
+.header_right{
+  float: right;
+  width: .60rem;
+}
+</style>
+
